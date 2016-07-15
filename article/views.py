3caef445b4b7106934436f295a1f843434be4a05
lichenjie <1194222721@qq.com>
2016-07-15 18:35:10 +0800

    all

---
+++ article/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from article.models import Article
+# Create your views here.
+def home(request):
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list':post_list})
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post':post})
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list':post_list, 'error':False})
+
+def about_me(request):
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category=tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list':post_list})
+
+
